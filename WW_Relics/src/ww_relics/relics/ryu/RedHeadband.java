8b0b7c8e876fa4aef639382268018b8f1043e04f
Clauvin <clauvin_almeida@hotmail.com>
2019-01-23 22:26:58 -0200

    (#15) Added one class to RedHeadband

--- WW_Relics/src/ww_relics/relics/ryu/RedHeadband.java
+++ WW_Relics/src/ww_relics/relics/ryu/RedHeadband.java
@@ -1,5 +1,6 @@
 package ww_relics.relics.ryu;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
