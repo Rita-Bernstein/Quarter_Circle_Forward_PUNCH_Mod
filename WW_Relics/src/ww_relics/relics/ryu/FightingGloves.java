9b51c988045ecce52ace34b65c3a0adb002bca05
Clauvin <clauvin_almeida@hotmail.com>
2019-01-29 23:42:17 -0200

    (#23) Started adding FightingGloves

---
+++ WW_Relics/src/ww_relics/relics/ryu/FightingGloves.java
@@ -0,0 +1,37 @@
+package ww_relics.relics.ryu;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.random.Random;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+import basemod.abstracts.CustomRelic;
+
+public class FightingGloves extends CustomRelic {
+	public static final String ID = "WW_Relics_Fighting_Gloves";
+	private static final int EXTRA_UPGRADES_PER_UPGRADE = 1;
+	
+	public FightingGloves() {
+		super(ID, "abacus.png", //add method for textures here.
+				RelicTier.RARE, LandingSound.SOLID);
+	}
+	
+	public String getUpdatedDescription() {
+		String description = "Something wrong happened, please warn the programmer!";
+		
+		if (EXTRA_UPGRADES_PER_UPGRADE == 1) {
+			description = DESCRIPTIONS[0] + EXTRA_UPGRADES_PER_UPGRADE +
+					DESCRIPTIONS[1];
+		} else if (EXTRA_UPGRADES_PER_UPGRADE > 1){
+			description = DESCRIPTIONS[0] + EXTRA_UPGRADES_PER_UPGRADE +
+					DESCRIPTIONS[2];
+		} 
+		return description;
+		
+	}
+
+	public AbstractRelic makeCopy() { // always override this method to return a new instance of your relic
+		return new FightingGloves();
+	}
+}
