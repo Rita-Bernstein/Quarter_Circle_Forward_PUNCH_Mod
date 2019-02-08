7183386482c9b5b03c319e5e94d12367bfa6ff44
Clauvin <clauvin_almeida@hotmail.com>
2019-02-08 02:36:44 -0200

    (#56) Let's see if this works...

--- WW_Relics/src/ww_relics/relics/ryu/DuffelBag.java
+++ WW_Relics/src/ww_relics/relics/ryu/DuffelBag.java
@@ -1,13 +1,16 @@
 package ww_relics.relics.ryu;
 
+import java.util.ArrayList;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.colorless.BandageUp;
 import com.megacrit.cardcrawl.cards.colorless.Panacea;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
+import com.megacrit.cardcrawl.rewards.RewardItem.RewardType;
 
 import basemod.abstracts.CustomRelic;
+import basemod.abstracts.CustomReward;
 
 public class DuffelBag extends CustomRelic {
 	public static final String ID = "WW_Relics:Duffel_Bag";
@@ -24,10 +27,24 @@ public class DuffelBag extends CustomRelic {
 	}
 	
 	public void OnEquip() {
-		 AbstractDungeon.effectList.add(
-				 new ShowCardAndObtainEffect(new BandageUp(), Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
-		 AbstractDungeon.effectList.add(
-				 new ShowCardAndObtainEffect(new Panacea(), Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
+		CustomReward duffel_bag_reward = new CustomReward(null, DESCRIPTIONS[2], RewardType.CARD) {
+			
+			@Override
+			public boolean claimReward() {
+				if(AbstractDungeon.screen == AbstractDungeon.CurrentScreen.COMBAT_REWARD) {
+					AbstractDungeon.cardRewardScreen.open(this.cards, this, "It works.");
+					AbstractDungeon.previousScreen = AbstractDungeon.CurrentScreen.COMBAT_REWARD;
+				}
+				return false;
+			}
+		};
+		
+		ArrayList<AbstractCard> list_of_cards = new ArrayList<AbstractCard>();
+		list_of_cards.add(new BandageUp());
+		list_of_cards.add(new Panacea());
+		
+		duffel_bag_reward.cards = list_of_cards;
+
 	}
 	
 	public AbstractRelic makeCopy() { // always override this method to return a new instance of your relic
