dcef5acd62eea047dc291523ba2295433eb341eb
Clauvin <clauvin_almeida@hotmail.com>
2019-02-24 14:41:28 -0300

    (#90) onEquip effects started

--- WW_Relics/src/ww_relics/relics/chun_li/SpikyBracers.java
+++ WW_Relics/src/ww_relics/relics/chun_li/SpikyBracers.java
@@ -1,8 +1,11 @@
 package ww_relics.relics.chun_li;
 
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.cards.CardGroup.CardGroupType;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase;
 
 import basemod.abstracts.CustomRelic;
 
@@ -13,11 +16,15 @@ public class SpikyBracers extends CustomRelic {
 	private static final int REDUCE_COST_BY = 1;
 	private static final int NUMBER_OF_CARDS_TO_APPLY_EFFECT = 2;
 	
+	private static AbstractCard[] cards_chosen;
+	
 	private static boolean effect_applied = false;
 	
 	public SpikyBracers() {
 		super(ID, "abacus.png", //add method for textures here.
 				RelicTier.UNCOMMON, LandingSound.HEAVY);
+		
+		cards_chosen = new AbstractCard[2];
 	}
 	
 	public String getUpdatedDescription() {
@@ -26,7 +33,72 @@ public class SpikyBracers extends CustomRelic {
 				DESCRIPTIONS[2];
 	}
 	
+	public void onEquip() {
+		
+		if (AbstractDungeon.player.masterDeck.getPurgeableCards().getAttacks().size() > 0)
+		{
+			if (AbstractDungeon.isScreenUp)
+			{
+				AbstractDungeon.dynamicBanner.hide();
+				AbstractDungeon.overlayMenu.cancelButton.hide();
+				AbstractDungeon.previousScreen = AbstractDungeon.screen;
+			}
+			
+			AbstractDungeon.getCurrRoom().phase = RoomPhase.INCOMPLETE;
+			
+			AbstractDungeon.gridSelectScreen.open(getValidCardGroup(), 2, getUpdatedDescription(), false, false, false, false);
+		}
+	}
+		
+	public CardGroup getValidCardGroup() {
+		
+		CardGroup valid_card_group = new CardGroup(CardGroupType.UNSPECIFIED);
+		
+		CardGroup powers = AbstractDungeon.player.masterDeck.getPowers();
+		CardGroup skills = AbstractDungeon.player.masterDeck.getSkills();
+		
+		for (int i = 0; i < powers.size(); i++) {
+			
+			if (powers.getNCardFromTop(i).cost >= 2) {
+				valid_card_group.addToTop(powers.getNCardFromTop(i));
+			}
+			
+		}
+		
+		for (int i = 0; i < skills.size(); i++) {
+			
+			if (skills.getNCardFromTop(i).cost >= 2) {
+				valid_card_group.addToTop(skills.getNCardFromTop(i));
+			}
+			
+		}
+		
+		return valid_card_group;
+		
+		
+	}
 	
+	public void onUnequip() {
+		
+	}
+	
+	/*public void update()
+	{
+		super.update();
+		if ((!this.cardSelected) && 
+		  (!AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()))
+		{
+			this.cardSelected = true;
+			this.card = ((AbstractCard)AbstractDungeon.gridSelectScreen.selectedCards.get(0));
+			this.card.inBottleFlame = true;
+			AbstractDungeon.getCurrRoom().phase = AbstractRoom.RoomPhase.COMPLETE;
+			AbstractDungeon.gridSelectScreen.selectedCards.clear();
+			this.description = (this.DESCRIPTIONS[2] + FontHelper.colorString(this.card.name, "y") + this.DESCRIPTIONS[3]);
+			this.tips.clear();
+			this.tips.add(new PowerTip(this.name, this.description));
+			initializeTips();
+	    }
+	}*/
 	
 	public boolean canSpawn()
 	{
