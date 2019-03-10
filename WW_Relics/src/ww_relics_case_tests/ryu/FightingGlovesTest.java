ea9b983a82eeee3341afbb78a43ed2de9bcf60b3
Clauvin <clauvin_almeida@hotmail.com>
2019-03-10 20:25:53 -0300

    (#113) Placing tests in the same project

--- WW_Relics/src/ww_relics_case_tests/ryu/FightingGlovesTest.java
+++ WW_Relics/src/ww_relics_case_tests/ryu/FightingGlovesTest.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.TestInfo;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
+import ww_relics.relics.ryu.FightingGloves;
+
 @RunWith(JUnitPlatform.class)
 public class FightingGlovesTest {
 
     @Test
-    @DisplayName("My First Test")
+    @DisplayName("Values added to the charge variable are really added - #113")
     void myFirstTest(TestInfo testInfo) {
+    	FightingGloves fighting_gloves = new FightingGloves();
         Assertions.assertEquals(2, 1+0, "1 + 1 = 2");
         Assertions.assertEquals("My First Test", testInfo.getDisplayName(),
                                     () -> "TestInfo is injected correctly");
