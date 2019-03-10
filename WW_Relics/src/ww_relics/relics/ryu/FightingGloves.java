aa3ecdf5f89096c8761bb36eae2c9799f908f323
Clauvin <clauvin_almeida@hotmail.com>
2019-03-10 20:17:21 -0300

    (#113) Creating positive_charges variable

--- WW_Relics/src/ww_relics/relics/ryu/FightingGloves.java
+++ WW_Relics/src/ww_relics/relics/ryu/FightingGloves.java
@@ -7,10 +7,35 @@ import basemod.abstracts.CustomRelic;
 public class FightingGloves extends CustomRelic {
 	public static final String ID = "WW_Relics:Fighting_Gloves";
 	private static final int EXTRA_UPGRADES_PER_UPGRADE = 1;
+	private static final int INITIAL_CHARGES = 0;
+	private static int positive_charges = INITIAL_CHARGES;
 	
 	public FightingGloves() {
 		super(ID, "abacus.png", //add method for textures here.
 				RelicTier.RARE, LandingSound.SOLID);
+	}
+	
+	public static int getCharges() {
+		return positive_charges;
+	}
+	
+	public static void setCharges(int value) {
+		positive_charges = value;
+		belowZeroCheck();
+	}
+	
+	public static void belowZeroCheck() {
+		if (positive_charges < 0) positive_charges = 0;
+	}
+	
+	public static void addCharges(int value_to_add) {
+		positive_charges += value_to_add;
+		belowZeroCheck();
+	}
+	
+	public static void removeCharges(int value_to_subtract) {
+		positive_charges -= value_to_subtract;
+		belowZeroCheck();
 	}
 	
 	@SuppressWarnings("unused")
