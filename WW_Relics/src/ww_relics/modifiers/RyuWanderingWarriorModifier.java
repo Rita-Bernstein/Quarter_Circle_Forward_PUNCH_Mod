bca1ebe1d8f9e81eee51b7f1d30a05ce376d5838
Clauvin <clauvin_almeida@hotmail.com>
2019-03-22 23:45:20 -0300

    (#148) Created a modifier class... now, how does this work?

---
+++ WW_Relics/src/ww_relics/modifiers/RyuWanderingWarriorModifier.java
@@ -0,0 +1,20 @@
+package ww_relics.modifiers;
+
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.daily.mods.AbstractDailyMod;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.RunModStrings;
+
+public class RyuWanderingWarriorModifier extends AbstractDailyMod {
+
+	public static final String ID = "ww_relics:WanderingWarrior";
+    private static final RunModStrings modStrings = CardCrawlGame.languagePack.getRunModString(ID);
+    public static final String NAME = modStrings.NAME;
+    public static final String DESC = modStrings.DESCRIPTION;
+	
+	public RyuWanderingWarriorModifier() {
+		super(ID, NAME, DESC, null, true);
+		this.img = ImageMaster.loadImage("images/relics/abacus.png");
+	}
+
+}
