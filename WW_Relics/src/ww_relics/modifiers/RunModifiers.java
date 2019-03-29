f11ca0f0e07ee66d9c6cdca3fe2192d5c659a365
Clauvin <clauvin_almeida@hotmail.com>
2019-03-28 23:56:44 -0300

    (#172) Created RunModifiers.java

---
+++ WW_Relics/src/ww_relics/modifiers/RunModifiers.java
@@ -0,0 +1,24 @@
+package ww_relics.modifiers;
+
+public class RunModifiers {
+
+	//Custom game modifiers
+	//...that add specific Street Fighter character's relics. 
+	public static final String WANDERING_WARRIOR_ID = "ww_relics:WanderingWarrior";
+	
+	public static final String BLUE_JADE_ID = "ww_relics:BlueJade";
+	
+	
+	//...that make the run more challenging.
+	public static final String WAIT_NO_REST_BETWEEN_ROUNDS_ID = "ww_relics:WaitNoRestBetweenRounds";
+	public static final int WAIT_NO_REST_BETWEEN_ROUNDS_STARTING_MAX_HP_PERCENTAGE = 65;
+	
+	public static final String FRESH_START_ID = "ww_relics:FreshStart";
+	
+	public static final String HALF_HEALTH_BAR_ID = "ww_relics:HalfHealthBar";
+	public static final int HALF_HEALTH_BAR_STARTING_MAX_HP_PERCENTAGE = 50;
+	
+	public static final String QUARTER_HEALTH_BAR_ID = "ww_relics:QuarterHealthBar";
+	public static final int QUARTER_HEALTH_BAR_STARTING_MAX_HP_PERCENTAGE = 25;
+	
+}
