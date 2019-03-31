1f3869f7757d571edd0bc03fd82f572e074312fd
Clauvin <clauvin_almeida@hotmail.com>
2019-03-31 02:28:35 -0300

    (#183) Relic img loading function done

--- WW_Relics/src/ww_relics/resources/relic_graphics/GraphicResources.java
+++ WW_Relics/src/ww_relics/resources/relic_graphics/GraphicResources.java
@@ -5,17 +5,18 @@ import com.megacrit.cardcrawl.helpers.ImageMaster;
 
 public class GraphicResources {
 
-	public static String graphic_resources_address;
+	public static final String GRAPHIC_RESOURCES_ADDRESS = "ww_relics/resources/";
+	public static final String RELIC_GRAPHICS_SUBADDRESS = "relic_graphics/";
 	
 	public static Texture img_red_headband;
 	public static Texture bg_img_red_headband;
 	
-	public GraphicResources(String address) {
-		graphic_resources_address = address;
-	}
-	
 	public void LoadGraphicResources() {
-		img_red_headband = ImageMaster.loadImage(graphic_resources_address);
+		String relic_full_address = GRAPHIC_RESOURCES_ADDRESS + RELIC_GRAPHICS_SUBADDRESS;
+		img_red_headband = ImageMaster.loadImage(relic_full_address +
+				"Red_Headband - headband-knot - Delapouite - CC BY 3.0.png");
+		bg_img_red_headband = ImageMaster.loadImage(relic_full_address +
+				"Red_Headband outline - headband-knot - Delapouite - CC BY 3.0.png");
 	}
 	
 }
