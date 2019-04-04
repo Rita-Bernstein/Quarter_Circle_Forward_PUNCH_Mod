10947e6101dc668ad4d4342e0b45e2ebcaf4e5fb
Clauvin <clauvin_almeida@hotmail.com>
2019-04-04 01:43:55 -0300

    And removed unnecessary imports

--- WW_Relics/src/ww_relics/relics/ryu/RedHeadband.java
+++ WW_Relics/src/ww_relics/relics/ryu/RedHeadband.java
@@ -1,8 +1,5 @@
 package ww_relics.relics.ryu;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
