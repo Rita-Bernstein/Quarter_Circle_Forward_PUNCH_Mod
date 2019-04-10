478dd502f69aad38427e9fd0cde44afc3b53d17b
Clauvin <clauvin_almeida@hotmail.com>
2019-04-10 12:59:55 -0300

    (#212) Removed unnecessary import

--- WW_Relics/src/ww_relics/actions/Refund1IfXCardSpentYOrMoreEnergyAction.java
+++ WW_Relics/src/ww_relics/actions/Refund1IfXCardSpentYOrMoreEnergyAction.java
@@ -8,8 +8,6 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
-import ww_relics.relics.chun_li.SpikyBracers;
-
 public class Refund1IfXCardSpentYOrMoreEnergyAction extends AbstractGameAction {
 
 	public AbstractCard card;
