1a61981bb9b7703469c39acfa6f47be15bbcf61e
Clauvin <clauvin_almeida@hotmail.com>
2019-04-18 23:45:12 -0300

    (#229) (#233) Undoing ReactiveGripPower creation

--- WW_Relics/src/ww_relics/relics/guile/ArmyBoots.java
+++ WW_Relics/src/ww_relics/relics/guile/ArmyBoots.java
@@ -9,7 +9,6 @@ import com.evacipated.cardcrawl.mod.stslib.relics.OnLoseBlockRelic;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -17,15 +16,15 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 import basemod.abstracts.CustomRelic;
-import ww_relics.powers.ReactiveGripPower;
 import ww_relics.resources.relic_graphics.GraphicResources;
 
-public class ArmyBoots extends CustomRelic  {
+public class ArmyBoots extends CustomRelic implements OnLoseBlockRelic  {
 	public static final String ID = "WW_Relics:Army_Boots";
 	
 	public static final Logger logger = LogManager.getLogger(ArmyBoots.class.getName());
 	
 	private static ArrayList<String> powers_affected_by_relic;
+	private static boolean relic_effect_activated = false;
 
 	public ArmyBoots() {
 		super(ID, GraphicResources.LoadRelicImage("White_Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
@@ -40,13 +39,12 @@ public class ArmyBoots extends CustomRelic  {
 		return "test";
 	}
 	
-	public void atBattleStartPreDraw() {
-	    AbstractDungeon.actionManager.addToBottom(
-	    		new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player,
-	    				new ReactiveGripPower(AbstractDungeon.player, 1), 1));
+	@Override
+	public void atPreBattle() {
+		relic_effect_activated = false;
 	}
 	
-	/*@Override
+	@Override
 	public int onLoseBlock(DamageInfo info, int damage_amount) {
 		
 		boolean found_power = false;
@@ -90,7 +88,7 @@ public class ArmyBoots extends CustomRelic  {
 		logger.info(info.base);
 		
 		return 0;
-	}*/
+	}	
 	
 	public AbstractRelic makeCopy() {
 		return new ArmyBoots();
