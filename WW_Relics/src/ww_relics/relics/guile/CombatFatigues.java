c73c1ab3cccd1db8ec47a33776dce8278f797f51
Clauvin <clauvin_almeida@hotmail.com>
2019-04-20 22:59:14 -0300

    (#244) Created block check

--- WW_Relics/src/ww_relics/relics/guile/CombatFatigues.java
+++ WW_Relics/src/ww_relics/relics/guile/CombatFatigues.java
@@ -1,5 +1,9 @@
 package ww_relics.relics.guile;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.badlogic.gdx.math.MathUtils;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 import basemod.abstracts.CustomRelic;
@@ -9,6 +13,11 @@ public class CombatFatigues extends CustomRelic {
 
 	public static final String ID = "WW_Relics:Combat_Fatigues";
 	
+	public static boolean gained_block_last_turn = false;
+	public static boolean is_first_turn = true;
+	
+	public static final Logger logger = LogManager.getLogger(CombatFatigues.class.getName()); 
+	
 	public CombatFatigues() {
 		super(ID, GraphicResources.LoadRelicImage("White_Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
 				RelicTier.RARE, LandingSound.FLAT);
@@ -17,6 +26,34 @@ public class CombatFatigues extends CustomRelic {
 	public String getUpdatedDescription() {
 		return "test";
 	}
+	
+	public int onPlayerGainedBlock(float blockAmount) {
+		
+		gained_block_last_turn = true;
+		
+		return MathUtils.floor(blockAmount);
+	}
+	
+	@Override	
+	public void atTurnStart() {
+		if (firstTurnPassed()) {
+			
+			logger.info(gained_block_last_turn);
+			
+		} else is_first_turn = false;
+		
+		resetConditionChecks();
+	}
+	
+	private boolean firstTurnPassed() {
+		return !is_first_turn;
+	}
+	
+	private void resetConditionChecks() {
+		gained_block_last_turn = false;
+	}
+	
+	
 	
 	@Override
 	public AbstractRelic makeCopy() {
