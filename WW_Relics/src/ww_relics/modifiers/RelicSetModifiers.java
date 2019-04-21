7b864da6c7977d472e660c8111154a2f45d7224d
Clauvin <clauvin_almeida@hotmail.com>
2019-04-21 13:07:12 -0300

    (#249) Added code for IndestructibleFortress run modifier

--- WW_Relics/src/ww_relics/modifiers/RelicSetModifiers.java
+++ WW_Relics/src/ww_relics/modifiers/RelicSetModifiers.java
@@ -6,6 +6,7 @@ import com.megacrit.cardcrawl.screens.custom.CustomMod;
 import com.megacrit.cardcrawl.unlock.UnlockTracker;
 
 import ww_relics.relics.chun_li.*;
+import ww_relics.relics.guile.*;
 import ww_relics.relics.ryu.*;
 
 public class RelicSetModifiers {
@@ -16,12 +17,16 @@ public class RelicSetModifiers {
 	
 	public static final String BLUE_JADE_ID = "ww_relics:BlueJade";
 	
+	public static final String INDESTRUCTIBLE_FORTRESS_ID = "ww_relics:IndestructibleFortress";
+	
 	public static void AddRelicSetModifiers(List<CustomMod> list) {
 		CustomMod wandering_warrior = new CustomMod(RelicSetModifiers.WANDERING_WARRIOR_ID, "y", true);
 		CustomMod blue_jade = new CustomMod(RelicSetModifiers.BLUE_JADE_ID, "y", true);
+		CustomMod indestructible_fortress = new CustomMod(RelicSetModifiers.INDESTRUCTIBLE_FORTRESS_ID, "y", true);
 		
 		list.add(wandering_warrior);
 		list.add(blue_jade);
+		list.add(indestructible_fortress);
 	}
 	
 	public static void AddWanderingWarriorRelicsToCustomRun(ArrayList<String> relics) {
@@ -40,6 +45,15 @@ public class RelicSetModifiers {
         UnlockTracker.markRelicAsSeen(WhiteBoots.ID);
         relics.add(Handcuffs.ID);
         UnlockTracker.markRelicAsSeen(Handcuffs.ID);
+	}
+	
+	public static void AddIndestructibleFortressToCustomRun(ArrayList<String> relics) {
+		relics.add(ArmyBoots.ID);
+		UnlockTracker.markRelicAsSeen(ArmyBoots.ID);
+		relics.add(ChainWithNametags.ID);
+		UnlockTracker.markRelicAsSeen(ChainWithNametags.ID);
+		relics.add(CombatFatigues.ID);
+		UnlockTracker.markRelicAsSeen(CombatFatigues.ID);
 	}
 	
 	
