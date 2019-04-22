12e21839c88a4242a5a0aae2633219dbd9848aa8
Clauvin <clauvin_almeida@hotmail.com>
2019-04-22 16:35:26 -0300

    (#255) Weakest Strike ready to be tested

--- WW_Relics/src/ww_relics/cards/dan/Strike_Dan_Weakest.java
+++ WW_Relics/src/ww_relics/cards/dan/Strike_Dan_Weakest.java
@@ -1,8 +1,15 @@
 package ww_relics.cards.dan;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -15,28 +22,43 @@ public class Strike_Dan_Weakest extends AbstractCard {
     private static final int COST = 0;
     private static final int ATTACK_DMG = 1;
 	
-    static {
-        cardStrings = CardCrawlGame.languagePack.getCardStrings("Strike_R");
-        NAME = Strike_Red.cardStrings.NAME;
-        DESCRIPTION = Strike_Red.cardStrings.DESCRIPTION;
+    public Strike_Dan_Weakest() {
+        super("WW_Relics:Strike_Dan_Weakest", Strike_Dan_Weakest.NAME,
+        		"red/attack/strike", COST, Strike_Dan_Weakest.DESCRIPTION,
+        		CardType.ATTACK, CardColor.COLORLESS, CardRarity.BASIC, CardTarget.ENEMY);
+        this.baseDamage = ATTACK_DMG;
+        this.tags.add(CardTags.STRIKE);
     }
+    
+    
     
 	@Override
 	public AbstractCard makeCopy() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Strike_Dan_Weakest();
 	}
 
 	@Override
 	public void upgrade() {
-		// TODO Auto-generated method stub
-
+		return;
 	}
 
 	@Override
-	public void use(AbstractPlayer arg0, AbstractMonster arg1) {
-		// TODO Auto-generated method stub
+	public void use(AbstractPlayer player, AbstractMonster monster) {
+        AbstractDungeon.actionManager.addToBottom(
+        		new DamageAction(monster, new DamageInfo(player, this.damage, this.damageTypeForTurn),
+        				AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        
+        //Forgetful effect should enter here, for now, Exhaust.
+       AbstractDungeon.actionManager.addToBottom(
+    		   new ExhaustSpecificCardAction(this, AbstractDungeon.player.hand));
+        		
 
 	}
+	
+	static {
+        cardStrings = CardCrawlGame.languagePack.getCardStrings("Strike_R");
+        NAME = Strike_Dan_Weakest.cardStrings.NAME;
+        DESCRIPTION = Strike_Dan_Weakest.cardStrings.DESCRIPTION;
+    }
 
 }
