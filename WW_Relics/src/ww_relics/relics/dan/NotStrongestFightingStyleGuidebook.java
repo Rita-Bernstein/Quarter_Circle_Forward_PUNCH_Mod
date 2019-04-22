f70f0ace173e3f2bc1001b053c47984c0efc3d30
Clauvin <clauvin_almeida@hotmail.com>
2019-04-22 00:19:47 -0300

    (#253) Refactored class name

--- WW_Relics/src/ww_relics/relics/dan/NotStrongestFightingStyleGuidebook.java
+++ WW_Relics/src/ww_relics/relics/dan/NotStrongestFightingStyleGuidebook.java
@@ -5,11 +5,11 @@ import com.megacrit.cardcrawl.relics.AbstractRelic;
 import basemod.abstracts.CustomRelic;
 import ww_relics.resources.relic_graphics.GraphicResources;
 
-public class NotStrongestFightingStyle extends CustomRelic {
+public class NotStrongestFightingStyleGuidebook extends CustomRelic {
 
-	public static final String ID = "WW_Relics:\"Strongest\"_Fighting_Style";
+	public static final String ID = "WW_Relics:Not_Strongest_Fighting_Style_Guidebook";
 	
-	public NotStrongestFightingStyle() {
+	public NotStrongestFightingStyleGuidebook() {
 		super(ID, GraphicResources.LoadRelicImage("White_Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
 				RelicTier.COMMON, LandingSound.FLAT);
 	}
@@ -19,6 +19,6 @@ public class NotStrongestFightingStyle extends CustomRelic {
 	}
 	
 	public AbstractRelic makeCopy() { 
-		return new NotStrongestFightingStyle();
+		return new NotStrongestFightingStyleGuidebook();
 	}
 }
