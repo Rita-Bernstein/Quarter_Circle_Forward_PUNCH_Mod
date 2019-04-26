b88b4a3b0b5b35844d99095aa76c98f5078b0257
Clauvin <clauvin_almeida@hotmail.com>
2019-04-26 01:28:28 -0300

    (#259) Code to add power done

--- WW_Relics/src/ww_relics/powers/PotentialPower.java
+++ WW_Relics/src/ww_relics/powers/PotentialPower.java
@@ -1,8 +1,10 @@
 package ww_relics.powers;
 
 import com.evacipated.cardcrawl.mod.stslib.powers.abstracts.TwoAmountPower;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
@@ -34,6 +36,25 @@ public class PotentialPower extends TwoAmountPower {
 	public void updateDescription()
 	{
 		this.description = numerator + "/" + denominator;
+	}
+	
+	
+	@Override
+	public void onApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
+
+		if ((power.ID == PotentialPower.POWER_ID) && (power.hashCode() != this.hashCode()) &&
+				(this.owner == power.owner)) {
+			PotentialPower the_new_potential = (PotentialPower) power;
+			
+			if (this.denominator == the_new_potential.denominator) {
+				this.numerator += the_new_potential.numerator;
+			} else {
+				//Add in the future code to sum fractions with different denominators
+			}
+			
+			AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction
+					(this.owner, this.owner, the_new_potential));
+		}
 	}
 	
 }
