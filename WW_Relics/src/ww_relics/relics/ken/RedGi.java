cb6000636c7cbe38982f01012dc5e454646861b6
Clauvin <clauvin_almeida@hotmail.com>
2019-04-27 07:47:54 -0300

    Added 2 empty relic class files for v0.10

---
+++ WW_Relics/src/ww_relics/relics/ken/RedGi.java
@@ -0,0 +1,12 @@
+package ww_relics.relics.ken;
+
+import basemod.abstracts.CustomRelic;
+
+public class RedGi extends CustomRelic {
+
+	public RedGi(String id, String imgName, RelicTier tier, LandingSound sfx) {
+		super(id, imgName, tier, sfx);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ WW_Relics/src/ww_relics/relics/ken/UnendingFlame.java
@@ -0,0 +1,12 @@
+package ww_relics.relics.ken;
+
+import basemod.abstracts.CustomRelic;
+
+public class UnendingFlame extends CustomRelic {
+
+	public UnendingFlame(String id, String imgName, RelicTier tier, LandingSound sfx) {
+		super(id, imgName, tier, sfx);
+		// TODO Auto-generated constructor stub
+	}
+
+}
