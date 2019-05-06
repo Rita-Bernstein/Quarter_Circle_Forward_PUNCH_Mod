93264d538c84836b8e4ac778974996e457e3f62e
Clauvin <clauvin_almeida@hotmail.com>
2019-05-05 22:27:28 -0300

    (#281) Class file done

--- WW_Relics/src/ww_relics/relics/ken/RedGi.java
+++ WW_Relics/src/ww_relics/relics/ken/RedGi.java
@@ -1,12 +1,33 @@
 package ww_relics.relics.ken;
 
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
 import basemod.abstracts.CustomRelic;
+import ww_relics.relics.guile.ArmyBoots;
+import ww_relics.resources.relic_graphics.GraphicResources;
 
 public class RedGi extends CustomRelic {
-
-	public RedGi(String id, String imgName, RelicTier tier, LandingSound sfx) {
-		super(id, imgName, tier, sfx);
-		// TODO Auto-generated constructor stub
+	public static final String ID = "WW_Relics:Red_Gi";
+	
+	
+	public RedGi() {
+		super(ID, GraphicResources.LoadRelicImage("White_Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
+				RelicTier.COMMON, LandingSound.FLAT);
+	}
+	
+	public String getUpdatedDescription() {
+		return DESCRIPTIONS[0];
+	}
+	
+	private void RelicVisualEffects() {
+		flash();
+		AbstractDungeon.actionManager.addToBottom(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+	}
+	
+	public AbstractRelic makeCopy() {
+		return new ArmyBoots();
 	}
 
 }
