2a8ba416209c84ad8eabdb483277ec03eb9f06a0
Clauvin <clauvin_almeida@hotmail.com>
2019-05-08 14:26:33 -0300

    (#283) Added relic to the game

--- WW_Relics/src/ww_relics/WW_Relics_Mod.java
+++ WW_Relics/src/ww_relics/WW_Relics_Mod.java
@@ -25,6 +25,7 @@ import ww_relics.relics.guile.ArmyBoots;
 import ww_relics.relics.guile.ChainWithNametags;
 import ww_relics.relics.guile.CombatFatigues;
 import ww_relics.relics.ken.BlackTrainingShirt;
+import ww_relics.relics.ken.RedGi;
 import ww_relics.relics.ryu.*;
 
 @SpireInitializer
@@ -132,6 +133,7 @@ public class WW_Relics_Mod implements AddCustomModeModsSubscriber, EditStringsSu
 	
 	private void addKenRelics() {
 		BaseMod.addRelic(new BlackTrainingShirt(), RelicType.SHARED);
+		BaseMod.addRelic(new RedGi(), RelicType.SHARED);
 	}
 	
 	private void addChunLiRelics() {
