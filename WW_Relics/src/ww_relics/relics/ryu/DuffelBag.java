27044c29d3b59cf4576a2c079d2d13836875c4e3
Clauvin <clauvin_almeida@hotmail.com>
2019-05-29 11:40:42 -0300

    (#332) Added save/load/clear functions

--- WW_Relics/src/ww_relics/relics/ryu/DuffelBag.java
+++ WW_Relics/src/ww_relics/relics/ryu/DuffelBag.java
@@ -1,7 +1,12 @@
 package ww_relics.relics.ryu;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireConfig;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.colorless.*;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -11,6 +16,7 @@ import com.megacrit.cardcrawl.rewards.RewardItem;
 import com.megacrit.cardcrawl.rooms.*;
 
 import basemod.abstracts.CustomRelic;
+import ww_relics.relics.character_cameos.sakura.SchoolBackpack;
 import ww_relics.resources.relic_graphics.GraphicResources;
 
 public class DuffelBag extends CustomRelic {
@@ -28,6 +34,8 @@ public class DuffelBag extends CustomRelic {
 	private int number_of_rewards_left;
 	
 	private boolean has_relic_been_used_this_battle = false;
+	
+	public static final Logger logger = LogManager.getLogger(SchoolBackpack.class.getName());
 	
 	public DuffelBag() {
 		super(ID, GraphicResources.LoadRelicImage("Duffel_Bag - swap-bag - Lorc - CC BY 3.0.png"),
@@ -139,6 +147,73 @@ public class DuffelBag extends CustomRelic {
 			
 		}
 		
+	}
+	
+	public static void save(final SpireConfig config) {
+
+        if (AbstractDungeon.player != null && AbstractDungeon.player.hasRelic(ID)) {
+    		logger.info("Started saving Duffel Bag information");
+
+    		if (AbstractDungeon.isDungeonBeaten || AbstractDungeon.player.isDead) {
+    			clear(config);
+    		} 
+    		else {
+    					
+                try {
+    				config.save();
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+                
+    		}
+
+            logger.info("Finished saving Duffel Bag information");
+        }
+        else {
+        	clear(config);
+        }
+
+    }
+	
+		public static void load(final SpireConfig config) {
+		
+		logger.info("Loading Duffel Bag info.");
+		if (AbstractDungeon.player.hasRelic(ID) && config.has("duffel_bag_1")) {
+			
+            try {
+				config.load();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            logger.info("Finished loading Duffel Bag info.");
+        }
+		
+		else
+		{
+			logger.info("There's no info, setting variables accordingly.");
+
+			logger.info("Finished setting Duffel Bag variables.");
+		}
+    }
+	
+	public static void loadCardRewardStored(final SpireConfig config) {
+		
+		int size = config.getInt("school_backpack_reward_size");
+		
+		for (int i = 0; i < size; i++) {
+			
+			card_reward_rarity.add(config.getString("school_backpack_reward_rarity_" + String.valueOf(i)));			
+			card_reward_id.add(config.getString("school_backpack_reward_" + String.valueOf(i)));
+			card_reward_upgrade.add(config.getBool("school_backpack_reward_upgrade_" + String.valueOf(i)));
+		
+		}
+	}
+	
+	public static void clear(final SpireConfig config) {
+		logger.info("Clearing Duffel Bag variables.");
+        
+        logger.info("Finished clearing Duffel Bag variables.");
 	}
 	
 	public AbstractRelic makeCopy() { // always override this method to return a new instance of your relic
