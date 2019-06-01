f61c23517f1e572378e2fcff65690650a3ff0191
Clauvin <clauvin_almeida@hotmail.com>
2019-05-31 23:19:30 -0300

    (#330) Added instrument patch for OutOfCombatPotion

---
+++ WW_Relics/src/ww_relics/patches/AddOutOfCombatPotionInterfacePatch.java
@@ -0,0 +1,31 @@
+package ww_relics.patches;
+
+import javassist.CannotCompileException;
+import javassist.NotFoundException;
+import javassist.expr.ExprEditor;
+import javassist.expr.Instanceof;
+import javassist.expr.MethodCall;
+
+public class AddOutOfCombatPotionInterfacePatch {
+
+	public static ExprEditor Instrument()
+	{
+		return new ExprEditor() {
+			public void edit(Instanceof i) throws CannotCompileException
+			{
+				try {
+					
+					if (i.getClass().equals("PotionPopUp")
+					        && i.getType().equals("FruitJuice"))
+						
+						i.replace("{ $r = OutOfCombatPotion; $_ = $proceed($$); }");
+					
+				} catch (NotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+	}
+	
+}
