81cde007009e93a45dd446d78bdc503975fc7d5a
Clauvin <clauvin_almeida@hotmail.com>
2019-05-31 23:52:36 -0300

    (#330) Created OutOfCombatPotion

--- WW_Relics/src/ww_relics/potions/OutOfCombatPotion.java
+++ WW_Relics/src/ww_relics/potions/OutOfCombatPotion.java
@@ -1,5 +1,33 @@
 package ww_relics.potions;
 
-public interface OutOfCombatPotion {
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
+
+import basemod.abstracts.CustomPotion;
+
+public class OutOfCombatPotion extends CustomPotion {
+
+	public OutOfCombatPotion(String name, String id, PotionRarity rarity, PotionSize size, PotionColor color) {
+		super(name, id, rarity, size, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getPotency(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public AbstractPotion makeCopy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void use(AbstractCreature arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
