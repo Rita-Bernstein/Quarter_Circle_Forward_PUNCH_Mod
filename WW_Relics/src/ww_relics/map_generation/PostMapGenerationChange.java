eded4841379ede1a8419c97eb19a7da6074395d3
Clauvin <clauvin_almeida@hotmail.com>
2019-06-12 22:18:43 -0300

    (#347) Created PostMapGenerationChange

---
+++ WW_Relics/src/ww_relics/map_generation/PostMapGenerationChange.java
@@ -0,0 +1,11 @@
+package ww_relics.map_generation;
+
+import ww_relics.interfaces.IPostMapGenerationAddStuff;
+
+public class PostMapGenerationChange {
+
+	public int counter;
+	
+	public IPostMapGenerationAddStuff post_map_gen_changer;
+	
+}
