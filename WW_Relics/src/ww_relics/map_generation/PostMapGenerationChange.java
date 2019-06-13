fd4c70f0b9fb5d2388a42f4c6d7ae8e5a27f329a
Clauvin <clauvin_almeida@hotmail.com>
2019-06-12 22:46:03 -0300

    (#347) Added Comparator to PostMapGenerationChange

--- WW_Relics/src/ww_relics/map_generation/PostMapGenerationChange.java
+++ WW_Relics/src/ww_relics/map_generation/PostMapGenerationChange.java
@@ -2,10 +2,15 @@ package ww_relics.map_generation;
 
 import ww_relics.interfaces.IPostMapGenerationAddStuff;
 
-public class PostMapGenerationChange {
+public class PostMapGenerationChange implements Comparable<PostMapGenerationChange> {
 
 	public int counter;
 	
 	public IPostMapGenerationAddStuff post_map_gen_changer;
+
+	@Override
+	public int compareTo(PostMapGenerationChange compared) {
+        return this.counter-compared.counter;
+    }
 	
 }
