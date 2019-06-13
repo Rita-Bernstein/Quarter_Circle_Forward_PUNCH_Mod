d9844bb1af61549f2832c964de37f2cf5e9acdc3
Clauvin <clauvin_almeida@hotmail.com>
2019-06-12 23:15:20 -0300

    (#347) Inverted order of functions

--- WW_Relics/src/ww_relics/interfaces/IPostMapGenerationAddStuff.java
+++ WW_Relics/src/ww_relics/interfaces/IPostMapGenerationAddStuff.java
@@ -2,8 +2,8 @@ package ww_relics.interfaces;
 
 public interface IPostMapGenerationAddStuff {
 
+	public boolean canDoAfterMapGeneration();
+	
 	public void doAfterMapGeneration();
 	
-	public boolean canDoAfterMapGeneration();
-
 }
