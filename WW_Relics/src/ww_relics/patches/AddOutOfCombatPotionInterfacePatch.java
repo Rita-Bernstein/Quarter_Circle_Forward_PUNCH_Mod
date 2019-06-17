3a8d5d48c266b97ced4c74cd2586418da22e6cc6
Clauvin <clauvin_almeida@hotmail.com>
2019-06-17 11:29:09 -0300

    (#361) Interface bug fixed

--- WW_Relics/src/ww_relics/patches/AddOutOfCombatPotionInterfacePatch.java
+++ WW_Relics/src/ww_relics/patches/AddOutOfCombatPotionInterfacePatch.java
@@ -8,27 +8,58 @@ import javassist.NotFoundException;
 import javassist.expr.ExprEditor;
 import javassist.expr.Instanceof;
 
-@SpirePatch(clz = PotionPopUp.class, method = "updateInput")
+
 public class AddOutOfCombatPotionInterfacePatch {
 
-	public static ExprEditor Instrument()
-	{
-		return new ExprEditor() {
-			@Override
-			public void edit(Instanceof i) throws CannotCompileException
-			{				
-				try {
-					
-					if (i.getType().getName().toString().equals("com.megacrit.cardcrawl.potions.FruitJuice"))
+	@SpirePatch(clz = PotionPopUp.class, method = "updateInput")
+	public static class UpdateInputUsable {
+		
+		public static ExprEditor Instrument()
+		{
+			return new ExprEditor() {
+				@Override
+				public void edit(Instanceof i) throws CannotCompileException
+				{				
+					try {
 						
-						i.replace("$_ = $proceed($$) || this.potion instanceof ww_relics.potions.OutOfCombatPotion;");
-					
-				} catch (NotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+						if (i.getType().getName().toString().equals("com.megacrit.cardcrawl.potions.FruitJuice"))
+							
+							i.replace("$_ = $proceed($$) || this.potion instanceof ww_relics.potions.OutOfCombatPotion;");
+						
+					} catch (NotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
-			}
-		};
+			};
+		}
+		
+	}
+	
+	@SpirePatch(clz = PotionPopUp.class, method = "render")
+	public static class RenderUsable {
+		
+		public static ExprEditor Instrument()
+		{
+			return new ExprEditor() {
+				
+				@Override
+				public void edit(Instanceof i) throws CannotCompileException
+				{				
+					try {
+						
+						if (i.getType().getName().toString().equals("com.megacrit.cardcrawl.potions.FruitJuice"))
+							
+							i.replace("$_ = $proceed($$) || (this.potion instanceof ww_relics.potions.OutOfCombatPotion && this.potion.canUse());");
+						
+					} catch (NotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			};
+		}
+		
 	}
 	
 }
