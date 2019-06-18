b6cd884ee5d8c958f9b89098d0c9e90ddde6ea3c
Clauvin <clauvin_almeida@hotmail.com>
2019-06-17 21:23:51 -0300

    (#363) Added Regen effect to the relic

--- WW_Relics/src/ww_relics/relics/mortal_kombat/NeverendingBlood.java
+++ WW_Relics/src/ww_relics/relics/mortal_kombat/NeverendingBlood.java
@@ -1,5 +1,12 @@
 package ww_relics.relics.mortal_kombat;
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.RegenPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
 import basemod.abstracts.CustomRelic;
 import ww_relics.resources.relic_graphics.GraphicResources;
 
@@ -7,9 +14,32 @@ public class NeverendingBlood extends CustomRelic {
 
 	public static final String ID = "WW_Relics:Neverending_Blood";
 	
+	
+	public static final int REGEN_AMOUNT = 1;
+	public static final int COUNTER_MAX_VALUE = 100;
+	
 	public NeverendingBlood() {
 		super(ID, GraphicResources.LoadRelicImage("White_Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
 				RelicTier.SPECIAL, LandingSound.MAGICAL);
+	}
+	
+	@Override
+	public int onAttacked(DamageInfo info, int damageAmount) {
+		
+		if (info.type == DamageType.NORMAL) {
+			
+			flash();
+			AbstractDungeon.actionManager.addToBottom(
+					new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player,
+							new RegenPower(AbstractDungeon.player, REGEN_AMOUNT))
+					);
+		}
+		
+		return damageAmount;
+	}
+	
+	public AbstractRelic makeCopy() {
+		return new NeverendingBlood();
 	}
 	
 }
