d75fdb04686d03b9d29903510833c1a343558f4e
Clauvin <clauvin_almeida@hotmail.com>
2019-06-19 03:05:47 -0300

    (#363) Nerfed(?) relic

--- WW_Relics/src/ww_relics/relics/mortal_kombat/NeverendingBlood.java
+++ WW_Relics/src/ww_relics/relics/mortal_kombat/NeverendingBlood.java
@@ -1,11 +1,10 @@
 package ww_relics.relics.mortal_kombat;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.RegenPower;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase;
 
 import basemod.abstracts.CustomRelic;
 import ww_relics.resources.relic_graphics.GraphicResources;
@@ -16,7 +15,7 @@ public class NeverendingBlood extends CustomRelic {
 	
 	public static final int REGEN_AMOUNT = 1;
 	public static final int COUNTER_MAX_VALUE = 100;
-	
+		
 	public NeverendingBlood() {
 		super(ID, GraphicResources.LoadRelicImage("White_Boots - steeltoe-boots - Lorc - CC BY 3.0.png"),
 				RelicTier.SPECIAL, LandingSound.MAGICAL);
@@ -28,20 +27,19 @@ public class NeverendingBlood extends CustomRelic {
 	}
 	
 	@Override
-	public int onAttacked(DamageInfo info, int damageAmount) {
+	public void onLoseHp(int damageAmount) {
 		
-		if (info.type == DamageType.NORMAL) {
+		if ((AbstractDungeon.getCurrRoom().phase == RoomPhase.COMBAT) && 
+			      (damageAmount > 0)) {
 			
 			flash();
 			AbstractDungeon.actionManager.addToBottom(
 					new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player,
-							new RegenPower(AbstractDungeon.player, REGEN_AMOUNT), REGEN_AMOUNT)
-			);
+							new RegenPower(AbstractDungeon.player, REGEN_AMOUNT), REGEN_AMOUNT));
 		}
 		
-		return damageAmount;
 	}
-		
+	
 	public AbstractRelic makeCopy() {
 		return new NeverendingBlood();
 	}
