c0ba0c8415193f06ac11fa83aff026ce6c90afba
Clauvin <clauvin_almeida@hotmail.com>
2019-06-23 03:26:52 -0300

    (#371) Created final base_game_player_classes

--- WW_Relics/src/ww_relics/WW_Relics_MiscelaneaCode.java
+++ WW_Relics/src/ww_relics/WW_Relics_MiscelaneaCode.java
@@ -1,10 +1,14 @@
 package ww_relics;
 
+import com.megacrit.cardcrawl.characters.AbstractPlayer.PlayerClass;
+
 public class WW_Relics_MiscelaneaCode {
 
 	public final static String infinite_spire_class_code = "infinitespire.InfiniteSpire";
 	public final static String replay_the_spire_class_code = "replayTheSpire.ReplayTheSpireMod";
-	
+	public final static PlayerClass[] base_game_player_classes =
+			{PlayerClass.IRONCLAD, PlayerClass.THE_SILENT, PlayerClass.DEFECT};
+
 	//just stole this code from ReplayTheSpire who stole this from blank lol
     public static boolean checkForMod(final String classPath) {
         try {
