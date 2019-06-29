5ac51219dd2289f86d32eebcf7ce370eed71f8c4
Clauvin <clauvin_almeida@hotmail.com>
2019-06-29 14:39:56 -0300

    (#374) Added names for option numbers

--- WW_Relics/src/ww_relics/events/act2/CrunchingNoisesEvent.java
+++ WW_Relics/src/ww_relics/events/act2/CrunchingNoisesEvent.java
@@ -17,6 +17,16 @@ public class CrunchingNoisesEvent extends AbstractImageEvent {
     public static final String TO_WHICH_ACT_ADD = TheCity.ID;
     public static final int WHERE_EVENT_TITLE_STARTS = 0;
     public static final int WHERE_EVENT_TEXT_STARTS = 9;
+    
+    public static final int EVENT_STARTING_POINT = 0;
+    public static final int ElITE_ENCOUNTER_PART_1 = 1;
+    public static final int ElITE_ENCOUNTER_PART_2_FIGHT = 2;
+    public static final int ELITE_VICTORIOUS_AFTERMATH = 3;
+    public static final int GAINED_BLOOD_RELIC = 4;
+    public static final int GAINED_SKELETON_RELIC = 5;
+    public static final int GAINED_CHALLENGER_COINS = 6;
+    public static final int GAINED_NOPE_NOPE_CANTALOPE_2_GOOD_INSTINCTS_PLUS = 7;
+    public static final int THE_SAFER_PATH_GAINED_GOOD_INSTINCTS = 8;
 
     public CrunchingNoisesEvent(){
         super(ID, "Crunching Noises", "images/events/cleric.jpg");
