f237a2d5bd9884efdc1e2249cbdd8092746850f5
Clauvin <clauvin_almeida@hotmail.com>
2019-07-18 15:33:18 -0300

    (#375) TiredGremlinNob skeleton done

--- WW_Relics/src/ww_relics/monsters/elites/TiredGremlinNob.java
+++ WW_Relics/src/ww_relics/monsters/elites/TiredGremlinNob.java
@@ -2,12 +2,17 @@ package ww_relics.monsters.elites;
 
 import com.badlogic.gdx.math.MathUtils;
 import com.esotericsoftware.spine.AnimationState;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.AnimateSlowAttackAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.MonsterStrings;
+import com.megacrit.cardcrawl.powers.AngerPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 
 import basemod.abstracts.CustomMonster;
@@ -23,6 +28,14 @@ public class TiredGremlinNob extends CustomMonster {
 	private static final float INITIAL_HEALTH_PERCENTAGE = 0.5f;
 	
 	private static final int INITIAL_STR_BUFF = 9;
+	private static final int INITIAL_ANGRY_BUFF = 2;
+	
+	private static final byte ANGRY_SCREAM = 1;
+	private static final byte ARM_SMASH = 2;
+	private static final byte BODY_BLOW = 3;
+	private static final byte HEAVY_BREATHING = 4;
+	
+	private static final int ANGRY_SCREAM_STR_BUFF = 2;
 	
 	
 	public TiredGremlinNob(float x, float y) {
@@ -43,25 +56,43 @@ public class TiredGremlinNob extends CustomMonster {
 	    setHp(HP_MIN, HP_MAX);
 	    
 	    this.currentHealth = (int)(this.currentHealth * INITIAL_HEALTH_PERCENTAGE);
+	    
+	    this.nextMove = 1;
 	}
-	
+
 	public void usePreBattleAction() {
         CardCrawlGame.music.unsilenceBGM();
         AbstractDungeon.scene.fadeOutAmbiance();
         AbstractDungeon.getCurrRoom().playBgmInstantly("ELITE");
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(this, this,
         		new StrengthPower(this, INITIAL_STR_BUFF)));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(this, this,
+        		new AngerPower(this, INITIAL_ANGRY_BUFF), INITIAL_ANGRY_BUFF));
     }
 	
 	@Override
 	protected void getMove(int arg0) {
-		// TODO Auto-generated method stub
-
+		AngryScreamIntent();
 	}
 
 	@Override
 	public void takeTurn() {
-		// TODO Auto-generated method stub
+		switch(this.nextMove) {
+			case ANGRY_SCREAM:
+				AngryScreamMove();
+				break;
+			case ARM_SMASH:
+				ArmSmashMove();
+				break;
+			case BODY_BLOW:
+				BodyBlowMove();
+				break;
+			case HEAVY_BREATHING:
+				HeavyBreathingMove();
+				break;
+			default:
+				
+		}
 
 	}
 	
@@ -76,16 +107,20 @@ public class TiredGremlinNob extends CustomMonster {
 	    } 
 	}
 	
-	public void AngryScreamingMove() {
-		
+	public void AngryScreamMove() {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(this, this,
+        		new StrengthPower(this, ANGRY_SCREAM_STR_BUFF)));		
 	}
 	
-	public void AngryScreamingIntent() {
-		
+	public void AngryScreamIntent() {
+        setMove(ANGRY_SCREAM, Intent.BUFF);
 	}
 	
 	public void ArmSmashMove() {
-		
+		playSfx();
+        AbstractDungeon.actionManager.addToBottom(new AnimateSlowAttackAction(this));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, (DamageInfo)this.damage
+              .get(0), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
 	}
 	
 	public void ArmSmashIntent() {
@@ -100,7 +135,7 @@ public class TiredGremlinNob extends CustomMonster {
 		
 	}
 	
-	public void HeavyBreeathingMove() {
+	public void HeavyBreathingMove() {
 		
 	}
 	
