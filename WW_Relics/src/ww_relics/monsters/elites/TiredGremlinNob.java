23a2ccd26ea3f6c92ddba8e224e08ce09020b59e
Clauvin <clauvin_almeida@hotmail.com>
2019-07-24 22:22:06 -0300

    (#375) HP now is at where it should be

--- WW_Relics/src/ww_relics/monsters/elites/TiredGremlinNob.java
+++ WW_Relics/src/ww_relics/monsters/elites/TiredGremlinNob.java
@@ -37,7 +37,7 @@ public class TiredGremlinNob extends CustomMonster {
 	public static final String[] DIALOG = monsterstrings.DIALOG;
 	private static final int HP_MIN = 180;
 	private static final int HP_MAX = 301;
-	private static final float INITIAL_HEALTH_PERCENTAGE = 0.5f;
+	private static final float INITIAL_HEALTH_PERCENTAGE = 1.0f;
 	
 	private static final int INITIAL_STR_BUFF = 5;
 	private static final int INITIAL_ANGRY_BUFF = 2;
