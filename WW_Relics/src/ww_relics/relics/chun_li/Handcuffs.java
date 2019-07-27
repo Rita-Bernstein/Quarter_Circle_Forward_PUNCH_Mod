888988cd81d1faae7a6023a8be73586dac373688
Clauvin <clauvin_almeida@hotmail.com>
2019-07-26 22:55:38 -0300

    (#388) Reduced the number of debuffs

--- WW_Relics/src/ww_relics/relics/chun_li/Handcuffs.java
+++ WW_Relics/src/ww_relics/relics/chun_li/Handcuffs.java
@@ -18,8 +18,8 @@ public class Handcuffs extends CustomRelic {
 	public static final String ID = "WW_Relics:Handcuffs";
 	
 	private static final int NUMBER_OF_USES_PER_FIGHT = 1;
-	private static final int NUMBER_OF_STR_DOWN_DEBUFFS = 2;
-	private static final int NUMBER_OF_UNSTEADY_DEBUFFS = 3;
+	private static final int NUMBER_OF_STR_DOWN_DEBUFFS = 0;
+	private static final int NUMBER_OF_UNSTEADY_DEBUFFS = 2;
 	private static final int NUMBER_OF_STUN_DEBUFFS = 1;
 	
 	public int number_of_uses_left_in_this_fight;
