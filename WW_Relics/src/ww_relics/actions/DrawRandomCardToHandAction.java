95732710a00b8bb2b45b65e08fa68c1e6710907b
Clauvin <clauvin_almeida@hotmail.com>
2019-07-29 17:41:46 -0300

    (#191) Created DrawRandomCardToHandAction

---
+++ WW_Relics/src/ww_relics/actions/DrawRandomCardToHandAction.java
@@ -0,0 +1,29 @@
+package ww_relics.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.random.Random;
+
+public class DrawRandomCardToHandAction extends AbstractGameAction {
+
+	private Random random;
+	private AbstractPlayer player;
+	
+	public DrawRandomCardToHandAction() {
+		
+		random = new Random();
+		player = AbstractDungeon.player;
+		
+	}
+
+	@Override
+	public void update() {
+    	int draw_pile_size = player.drawPile.group.size();
+    	int which_card = random.random(0, draw_pile_size-1);
+    	AbstractCard the_card = player.drawPile.getNCardFromTop(which_card);
+    	player.drawPile.moveToHand(the_card, player.drawPile);
+	}
+	
+}
