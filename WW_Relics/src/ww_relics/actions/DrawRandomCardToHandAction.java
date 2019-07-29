596039f69bbac53749c3d282ab57f289e09b5cd3
Clauvin <clauvin_almeida@hotmail.com>
2019-07-29 17:56:47 -0300

    (#397) Randomizer is from the game now

--- WW_Relics/src/ww_relics/actions/DrawRandomCardToHandAction.java
+++ WW_Relics/src/ww_relics/actions/DrawRandomCardToHandAction.java
@@ -4,16 +4,13 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.random.Random;
 
 public class DrawRandomCardToHandAction extends AbstractGameAction {
 
-	private Random random;
 	private AbstractPlayer player;
 	
 	public DrawRandomCardToHandAction() {
 		
-		random = new Random();
 		player = AbstractDungeon.player;
 		
 	}
@@ -23,7 +20,7 @@ public class DrawRandomCardToHandAction extends AbstractGameAction {
 		
 		if (!this.isDone) {
 	    	int draw_pile_size = player.drawPile.group.size();
-	    	int which_card = random.random(0, draw_pile_size-1);
+	    	int which_card = AbstractDungeon.cardRandomRng.random(0, draw_pile_size-1);
 	    	AbstractCard the_card = player.drawPile.getNCardFromTop(which_card);
 	    	player.drawPile.moveToHand(the_card, player.drawPile);
 	    	
