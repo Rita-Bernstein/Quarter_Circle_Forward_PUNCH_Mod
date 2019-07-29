3cf96709ae891d4e68dbc01a4e010f77366e5876
Clauvin <clauvin_almeida@hotmail.com>
2019-07-29 18:19:21 -0300

    (#399) Created EasierRunModifiers

---
+++ WW_Relics/src/ww_relics/modifiers/EasierRunModifiers.java
@@ -0,0 +1,34 @@
+package ww_relics.modifiers;
+
+import java.util.List;
+
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.screens.custom.CustomMod;
+
+public class EasierRunModifiers {
+
+	public static final String LOWERING_THE_HANDICAP_ID = "ww_relics:LoweringTheHandicap";
+	public static final int LOWERING_THE_HANDICAP_STARTING_HEALTH_PERCENTAGE = 125;
+	
+	public static final void AddEasierSetModifiers(List<CustomMod> list) {
+		
+		CustomMod lowering_the_handicap = 
+				new CustomMod(HarderRunModifiers.WAIT_NO_REST_BETWEEN_ROUNDS_ID, "y", true);
+		
+		list.add(lowering_the_handicap);
+	}
+	
+	public static void AddLoweringTheHandicaptHealthBarEffectsToRun() {
+		multiplyMaxHPByNewPercentage(EasierRunModifiers.LOWERING_THE_HANDICAP_STARTING_HEALTH_PERCENTAGE);
+	}
+	
+	public static void multiplyMaxHPByNewPercentage(float new_percentage) {
+    	float new_startingMaxHP = 
+    			AbstractDungeon.player.startingMaxHP *
+    			new_percentage / 100;
+        AbstractDungeon.player.startingMaxHP = (int)new_startingMaxHP;
+        AbstractDungeon.player.maxHealth = (int)new_startingMaxHP;
+        AbstractDungeon.player.currentHealth = AbstractDungeon.player.maxHealth;
+    }
+	
+}
