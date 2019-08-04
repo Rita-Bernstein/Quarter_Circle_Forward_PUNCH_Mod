0ba4c253cff9ef1ec4dd6e392291b304bdea1ca9
Clauvin <clauvin_almeida@hotmail.com>
2019-08-04 14:34:49 -0300

    (#410) Postfix: why this does not work .-.

--- WW_Relics/src/ww_relics/patches/MakeChallengerCoinALimitedPotionPatch.java
+++ WW_Relics/src/ww_relics/patches/MakeChallengerCoinALimitedPotionPatch.java
@@ -1,8 +1,5 @@
 package ww_relics.patches;
 
-import java.lang.reflect.Field;
-
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
@@ -15,25 +12,15 @@ public class MakeChallengerCoinALimitedPotionPatch {
 			paramtypez = {AbstractPotion.PotionRarity.class,
 						  boolean.class}
 			)
-	public static class ReturnRandomPotionButNotChallengerCoin {
+	public static class ReturnRandomPotion {
 		
-		@SpireInsertPatch(
-				rloc=8,
-				localvars={"temp", "spamCheck"}
-			)
-		public static void Insert(AbstractDungeon __instance, AbstractPotion param1, boolean param2){	
-			try {
-				Field ori_temp = __instance.getClass().getDeclaredField("temp");
-				AbstractPotion temp = (AbstractPotion) ori_temp.get(__instance);
-				if (temp.ID != "WW_Relics:Challenger_Coin") {
-					Field spamCheck = __instance.getClass().getDeclaredField("spamCheck");
-					spamCheck.set(__instance, false);
-			    }
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
+		public static AbstractPotion Postfix(AbstractPotion __result, AbstractPotion.PotionRarity rarity,
+												 boolean limited){	
+			if (!limited) return __result;
+			else if (__result.ID != "WW_Relics:Challenger_Coin") return __result;
+			else return AbstractDungeon.returnRandomPotion(rarity, limited);
+
 		}
-		
 	}
 	
 }
