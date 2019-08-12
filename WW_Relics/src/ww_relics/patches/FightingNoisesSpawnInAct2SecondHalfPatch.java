fe04cf517c7967bb1b83cb645333f465e7a0f89d
Clauvin <clauvin_almeida@hotmail.com>
2019-08-11 21:52:44 -0300

    (#381) To-test-now patch for the event

---
+++ WW_Relics/src/ww_relics/patches/FightingNoisesSpawnInAct2SecondHalfPatch.java
@@ -0,0 +1,37 @@
+package ww_relics.patches;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.events.AbstractEvent;
+
+import ww_relics.WW_Relics_Mod;
+import ww_relics.events.act2.FightingNoisesEvent;
+
+public class FightingNoisesSpawnInAct2SecondHalfPatch {
+
+	public static final Logger logger =
+			LogManager.getLogger(FightingNoisesSpawnInAct2SecondHalfPatch.class.getName());
+	
+	@SpirePatch(cls = "com.megacrit.cardcrawl.dungeons.AbstractDungeon", method = "getEvent")
+	public static class GetEventPatch {
+
+	    @SpirePostfixPatch
+	    public static AbstractEvent FightingNoisesSpawnInAct2SecondHalf(
+	        AbstractEvent _retVal,
+	        com.megacrit.cardcrawl.random.Random rng) {
+
+	    if ((_retVal instanceof FightingNoisesEvent) &&
+	    	( AbstractDungeon.currMapNode.y > AbstractDungeon.map.size() / 2)) {
+	    	logger.info("ASASDASFAF Here");
+	    	return AbstractDungeon.getEvent(AbstractDungeon.eventRng);
+	    }
+	    		
+	    else return _retVal;
+	    
+	}
+
+}
