b188eef31635f4e61a1ad2677c51e3fceb19e3c1
Clauvin <clauvin_almeida@hotmail.com>
2019-08-12 01:05:03 -0300

    (#385) Removing other logger lines

--- WW_Relics/src/ww_relics/patches/FightingNoisesSpawnInAct2SecondHalfPatch.java
+++ WW_Relics/src/ww_relics/patches/FightingNoisesSpawnInAct2SecondHalfPatch.java
@@ -1,8 +1,5 @@
 package ww_relics.patches;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -11,9 +8,6 @@ import com.megacrit.cardcrawl.events.AbstractEvent;
 import ww_relics.events.act2.FightingNoisesEvent;
 
 public class FightingNoisesSpawnInAct2SecondHalfPatch {
-
-	public static final Logger logger =
-			LogManager.getLogger(FightingNoisesSpawnInAct2SecondHalfPatch.class.getName());
 	
 	@SpirePatch(cls = "com.megacrit.cardcrawl.dungeons.AbstractDungeon", method = "getEvent")
 	public static class GetEventPatch {
@@ -24,7 +18,6 @@ public class FightingNoisesSpawnInAct2SecondHalfPatch {
 	        com.megacrit.cardcrawl.random.Random rng) {
 			    if ((_retVal instanceof FightingNoisesEvent) &&
 			    	(AbstractDungeon.currMapNode.y > AbstractDungeon.map.size() / 2)) {
-			    	logger.info("ASASDASFAF Here");
 			    	return _retVal;
 			    	
 			    }
