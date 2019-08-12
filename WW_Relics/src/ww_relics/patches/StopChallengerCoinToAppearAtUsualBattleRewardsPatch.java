0a9d16894ca975ead6d462ef43bbd05b138f093f
Clauvin <clauvin_almeida@hotmail.com>
2019-08-12 00:29:58 -0300

    (#385) Created StopChallengerCoinToAppearAtUsualBattleRewardsPatch

---
+++ WW_Relics/src/ww_relics/patches/StopChallengerCoinToAppearAtUsualBattleRewardsPatch.java
@@ -0,0 +1,22 @@
+package ww_relics.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rewards.RewardItem;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+import ww_relics.potions.ChallengerCoin;
+
+@SpirePatch(clz= AbstractRoom.class, method = "generateMap")
+public class StopChallengerCoinToAppearAtUsualBattleRewardsPatch {
+
+	@SpireInsertPatch(rloc=25)
+	public static void Insert(AbstractRoom __instance) {
+		while(__instance.rewards.get(__instance.rewards.size() - 1).potion.ID ==
+				ChallengerCoin.ID) {
+			__instance.rewards.remove(__instance.rewards.size() - 1);
+			__instance.rewards.add(new RewardItem(AbstractDungeon.returnRandomPotion()));
+		}
+	}
+}
