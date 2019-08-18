d4527ddb1e501166a081e50cef60d363a9bb1f6c
Clauvin <clauvin_almeida@hotmail.com>
2019-08-18 14:14:43 -0300

    (#448) Added KeywordWithProper class

---
+++ WW_Relics/src/ww_relics/KeywordWithProper.java
@@ -0,0 +1,13 @@
+package ww_relics;
+
+//copy/pasted from Chen mod
+public class KeywordWithProper {
+
+    public String PROPER_NAME;
+    public String DESCRIPTION;
+    public String[] NAMES;
+
+    public KeywordWithProper() {
+    }
+    
+}
