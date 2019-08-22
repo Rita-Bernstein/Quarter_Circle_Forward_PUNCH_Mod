9903fdde343fd812733ae00942ce0fde274754a2
Clauvin <clauvin_almeida@hotmail.com>
2019-08-22 19:42:11 -0300

    (#457) Created returnLocalizationMainFolder()

--- WW_Relics/src/ww_relics/QCFPunch_MiscCode.java
+++ WW_Relics/src/ww_relics/QCFPunch_MiscCode.java
@@ -38,6 +38,10 @@ public class QCFPunch_MiscCode {
 				  + "\r\n Most of the images in the mod are temporary and will be substituted/improved on v1.0.";
 	}
 	
+	public static String returnLocalizationMainFolder() {
+		return "ww_relics/localization/";
+	}
+	
 	//Yes, this is me not wanting to go raw to the ShopScreen.initPotions class.
 	public static void resetNumberOfChallengerCoinPotionsVariable() {
 		number_of_challenger_coin_potions_at_shop = 0;
