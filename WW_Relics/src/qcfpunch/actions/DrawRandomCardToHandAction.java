f2f5c9d4ce9209b812eaf0b4653173ed0cdcc365
Clauvin <clauvin_almeida@hotmail.com>
2019-08-25 23:28:50 -0300

    (#458) Removed unecessary logger lines

--- WW_Relics/src/qcfpunch/actions/DrawRandomCardToHandAction.java
+++ WW_Relics/src/qcfpunch/actions/DrawRandomCardToHandAction.java
@@ -1,21 +1,14 @@
 package qcfpunch.actions;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
-import qcfpunch.relics.ryu.FightingGloves;
-
 public class DrawRandomCardToHandAction extends AbstractGameAction {
 
 	private AbstractPlayer player;
-	
-	public static final Logger logger = LogManager.getLogger(FightingGloves.class.getName());
-	
+		
 	public DrawRandomCardToHandAction() {
 		
 		player = AbstractDungeon.player;
@@ -34,7 +27,6 @@ public class DrawRandomCardToHandAction extends AbstractGameAction {
 
 	    	if (draw_pile_size > 0) {
 		    	which_card %= draw_pile_size;
-		    	logger.info(which_card);
 		    	AbstractCard the_card = player.drawPile.getNCardFromTop(which_card);
 		    	player.drawPile.moveToHand(the_card, player.drawPile);
 	    	}
