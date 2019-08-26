9d217401f8638e8ea46fd12f5cca8331cf04d344
Clauvin <clauvin_almeida@hotmail.com>
2019-08-25 23:18:56 -0300

    (#458) Fixed divided by zero exception

--- WW_Relics/src/qcfpunch/actions/DrawRandomCardToHandAction.java
+++ WW_Relics/src/qcfpunch/actions/DrawRandomCardToHandAction.java
@@ -1,13 +1,20 @@
 package qcfpunch.actions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
+import qcfpunch.relics.ryu.FightingGloves;
+
 public class DrawRandomCardToHandAction extends AbstractGameAction {
 
 	private AbstractPlayer player;
+	
+	public static final Logger logger = LogManager.getLogger(FightingGloves.class.getName());
 	
 	public DrawRandomCardToHandAction() {
 		
@@ -24,10 +31,14 @@ public class DrawRandomCardToHandAction extends AbstractGameAction {
 	    	int which_card = (int)AbstractDungeon.cardRandomRng.randomLong();
 	    	
 	    	if (which_card <0) which_card *= -1;
-	    	
-	    	which_card %= draw_pile_size;
-	    	AbstractCard the_card = player.drawPile.getNCardFromTop(which_card);
-	    	player.drawPile.moveToHand(the_card, player.drawPile);
+
+	    	if (draw_pile_size > 0) {
+		    	which_card %= draw_pile_size;
+		    	logger.info(which_card);
+		    	AbstractCard the_card = player.drawPile.getNCardFromTop(which_card);
+		    	player.drawPile.moveToHand(the_card, player.drawPile);
+	    	}
+
 	    	
 	    	this.isDone = true;
 		}
