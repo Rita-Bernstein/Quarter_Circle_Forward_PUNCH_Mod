a776008e95e4d0a4cc19c7868786f3f99db93d33
Clauvin <clauvin_almeida@hotmail.com>
2019-08-25 23:08:58 -0300

    (#458) Just to be sure

--- WW_Relics/src/qcfpunch/actions/DrawRandomCardToHandAction.java
+++ WW_Relics/src/qcfpunch/actions/DrawRandomCardToHandAction.java
@@ -20,7 +20,11 @@ public class DrawRandomCardToHandAction extends AbstractGameAction {
 		
 		if (!this.isDone) {
 	    	int draw_pile_size = player.drawPile.group.size();
+	    	
 	    	int which_card = (int)AbstractDungeon.cardRandomRng.randomLong();
+	    	
+	    	if (which_card <0) which_card *= -1;
+	    	
 	    	which_card %= draw_pile_size;
 	    	AbstractCard the_card = player.drawPile.getNCardFromTop(which_card);
 	    	player.drawPile.moveToHand(the_card, player.drawPile);
