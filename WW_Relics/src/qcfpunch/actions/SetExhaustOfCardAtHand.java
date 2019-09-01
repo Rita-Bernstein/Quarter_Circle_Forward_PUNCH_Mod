ffb7385b737cd2de6c657a6910b656a775ee7e33
Clauvin <clauvin_almeida@hotmail.com>
2019-08-31 21:04:28 -0300

    (#474) SetExhaustOfCardAtHand ready to test

--- WW_Relics/src/qcfpunch/actions/SetExhaustOfCardAtHand.java
+++ WW_Relics/src/qcfpunch/actions/SetExhaustOfCardAtHand.java
@@ -1,10 +1,39 @@
 package qcfpunch.actions;
 
+import java.util.UUID;
+
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 public class SetExhaustOfCardAtHand extends AbstractGameAction  {
 
-	public SetExhaustOfCardAtHand(UUID uuid, )
+	UUID uuid;
+	boolean exhausts;
+	
+	public SetExhaustOfCardAtHand(UUID uuid, boolean it_exhausts) {
+		this.uuid = uuid;
+		this.exhausts = it_exhausts;
+	}
+	
+	@Override
+	public void update() {
+		
+		if (!this.isDone) {
+			
+			CardGroup hand = AbstractDungeon.player.hand;
+			
+			for (int i = 0; i < hand.size(); i++) {
+				if (hand.getNCardFromTop(i).uuid == this.uuid) {
+					hand.getNCardFromTop(i).exhaust = this.exhausts;
+					break;
+				}
+			}
+			
+			this.isDone = true;
+		}
+
+	}
 	
 	//Find card
 		//By name
